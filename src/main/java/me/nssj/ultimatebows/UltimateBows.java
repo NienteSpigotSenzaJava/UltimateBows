0d1afa33f2c3c349a56a540b38c79dc725dc2e4a
Patrick260 <Patrick260@protonmail.com>
2023-04-01 16:01:50 +0200

    Refactor UltimateBows.java

--- src/main/java/me/nssj/ultimatebows/UltimateBows.java
+++ src/main/java/me/nssj/ultimatebows/UltimateBows.java
@@ -13,6 +13,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class UltimateBows extends JavaPlugin {
 
     private ConsoleCommandSender console;
+    
     private PluginManager pluginManager;
 
     @Override
@@ -21,7 +22,6 @@ public final class UltimateBows extends JavaPlugin {
         console = getServer().getConsoleSender();
 
         pluginManager = getServer().getPluginManager();
-
         pluginManager.registerEvents(new ArrowListener(), this);
 
         getCommand("ubows").setExecutor(new UBowsCommand());
