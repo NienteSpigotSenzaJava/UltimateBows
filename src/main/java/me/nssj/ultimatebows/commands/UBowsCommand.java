77e45d5fe6674d07102e5c190b764b862f090a0e
Patrick260 <Patrick260@protonmail.com>
2023-04-01 16:11:01 +0200

    Refactor UBowsCommand.java

--- src/main/java/me/nssj/ultimatebows/commands/UBowsCommand.java
+++ src/main/java/me/nssj/ultimatebows/commands/UBowsCommand.java
@@ -3,6 +3,7 @@ package me.nssj.ultimatebows.commands;
 import me.nssj.ultimatebows.bows.Bow;
 import me.nssj.ultimatebows.bows.BowsManager;
 
+import me.nssj.ultimatebows.utils.Util;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -14,45 +15,60 @@ import org.bukkit.util.StringUtil;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class UBowsCommand implements CommandExecutor, TabCompleter {
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand(final CommandSender sender, final Command command, final String label, final String[] args) {
+
         if (sender instanceof Player) {
-            Player player = (Player) sender;
 
-            if (args.length != 2) {
-                player.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "Syntax: /ubows give <bowName>");
-                return true;
-            }
+            final Player player = (Player) sender;
 
-            if (args[0].equalsIgnoreCase("give")) {
-                Bow bow = BowsManager.getBowByName(args[1]);
+            if (args.length == 2 && args[0].equalsIgnoreCase("give")) {
+
+                final Bow bow = BowsManager.getBowByName(args[1]);
 
                 if (BowsManager.getBows().contains(bow)) {
+
                     ItemStack bowItem = BowsManager.getBowItem(bow);
                     player.getInventory().addItem(bowItem);
+
                 } else {
-                    player.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "Syntax: /ubows give <bowName>");
+
+                    player.sendMessage(Util.getColorizedText(ChatColor.RED,true,"Syntax: /ubows give <bowName>"));
+
                 }
+
             } else {
-                player.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "Syntax: /ubows give <bowName>");
+
+                player.sendMessage(Util.getColorizedText(ChatColor.RED,true,"Syntax: /ubows give <bowName>"));
+
             }
+
         }
 
         return true;
+
     }
 
     @Override
-    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+    public List<String> onTabComplete(final CommandSender sender, final Command command, final String alias, final String[] args) {
+
         if (args.length == 1) {
-            return StringUtil.copyPartialMatches(args[0], Arrays.asList("give"), new ArrayList<>());
+
+            return StringUtil.copyPartialMatches(args[0], Collections.singletonList("give"), new ArrayList<>());
+
         } else if (args.length == 2 && args[0].equalsIgnoreCase("give")) {
+
             return StringUtil.copyPartialMatches(args[1], Arrays.asList("destructionBow", "playerBow", "teleportBow", "explosiveBow", "waterBow", "lavaBow"), new ArrayList<>());
+
         }
 
         return null;
+
     }
+
 }
